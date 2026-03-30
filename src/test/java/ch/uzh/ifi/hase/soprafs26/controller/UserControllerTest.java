2810893416b87a7de80fb55a253ddebc78671935
stella-sy-x <xsystella@gmail.com>
2026-03-30 22:02:33 +0200

    fix: remove getName, setName from User tests; add setPassword for User tests; update default UserStatus to ONLINE

--- src/test/java/ch/uzh/ifi/hase/soprafs26/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs26/controller/UserControllerTest.java
@@ -50,7 +50,6 @@ public class UserControllerTest {
 	public void givenUsers_whenGetUsers_thenReturnJsonArray() throws Exception {
 		// given
 		User user = new User();
-		user.setName("Firstname Lastname");
 		user.setUsername("firstname@lastname");
 		user.setStatus(UserStatus.OFFLINE);
 
@@ -66,7 +65,6 @@ public class UserControllerTest {
 		// then
 		mockMvc.perform(getRequest).andExpect(status().isOk())
 				.andExpect(jsonPath("$", hasSize(1)))
-				.andExpect(jsonPath("$[0].name", is(user.getName())))
 				.andExpect(jsonPath("$[0].username", is(user.getUsername())))
 				.andExpect(jsonPath("$[0].status", is(user.getStatus().toString())));
 	}
@@ -76,13 +74,11 @@ public class UserControllerTest {
 		// given
 		User user = new User();
 		user.setId(1L);
-		user.setName("Test User");
 		user.setUsername("testUsername");
 		user.setToken("1");
 		user.setStatus(UserStatus.ONLINE);
 
 		UserPostDTO userPostDTO = new UserPostDTO();
-		userPostDTO.setName("Test User");
 		userPostDTO.setUsername("testUsername");
 
 		given(userService.createUser(Mockito.any())).willReturn(user);
@@ -96,7 +92,6 @@ public class UserControllerTest {
 		mockMvc.perform(postRequest)
 				.andExpect(status().isCreated())
 				.andExpect(jsonPath("$.id", is(user.getId().intValue())))
-				.andExpect(jsonPath("$.name", is(user.getName())))
 				.andExpect(jsonPath("$.username", is(user.getUsername())))
 				.andExpect(jsonPath("$.status", is(user.getStatus().toString())));
 	}
