54ea54a4f9c178b0f764df942e24085d6ed1a003
dogamentese <dogamentese@gmail.com>
2026-03-30 14:19:55 +0200

    feat: implement registration endpoint with unique username validation #1 #16

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/User.java
@@ -26,9 +26,6 @@ public class User implements Serializable {
 	@GeneratedValue
 	private Long id;
 
-	@Column(nullable = false)
-	private String name;
-
 	@Column(nullable = false, unique = true)
 	private String username;
 
@@ -40,6 +37,7 @@ public class User implements Serializable {
 
 	@Column(nullable = false)
 	private String password;
+	
 
 	public Long getId() {
 		return id;
@@ -47,14 +45,6 @@ public class User implements Serializable {
 
 	public void setId(Long id) {
 		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 	public String getUsername() {
